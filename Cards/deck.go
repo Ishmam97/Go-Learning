cc8aa9beb3783f2534a7e4e3446d50407f6aed32
Ishmam97 <43916219+Ishmam97@users.noreply.github.com>
2021-02-04 11:46:23 +0600

    added shuffle and deal functions

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,11 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Clovers", "Diamonds"}
+	cardVals := []string{"ACE", "KING", "QUEEN", "JACK", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"}
+	for _, suit := range cardSuits {
+		for _, val := range cardVals {
+			cards = append(cards, val+" of "+suit)
+		}
+	}
+	return cards
+}
+func (d deck) shuffle() {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(d), func(i, j int) { d[i], d[j] = d[j], d[i] })
+}
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,11 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := deck{}
-
-	cards = append(cards, "Six of spades")
+	cards := newDeck()
+	cards.shuffle()
+	fmt.Println("len: ", len(cards))
+	var hand deck
+	hand, cards = deal(cards, 5)
+	fmt.Println("####HAND####")
+	hand.print()
+	fmt.Println("hand len: ", len(hand))
+	fmt.Println("####DECK####")
 	cards.print()
-
+	fmt.Println("deck len: ", len(cards))
 }
 func newCard() string {
 	return "5 of diamonds"
