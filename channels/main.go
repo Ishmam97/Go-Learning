2f0058a2d2fbd08226ed8c59542fb3d0c617f6d3
Ishmam97 <43916219+Ishmam97@users.noreply.github.com>
2021-02-06 16:47:06 +0600

    Update main.go

--- channels/main.go
+++ channels/main.go
@@ -23,16 +23,19 @@ func main() {
 	// et := time.Now()
 	// fmt.Println("time taken for sequential = ", et.Sub(st))
 	//go routine
-	st := time.Now()
+	// st := time.Now()
 	for _, link := range links {
 		go seqCheckLink(link, c)
 
 	}
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for l := range c {
+		go func(l string) {
+			time.Sleep(3 * time.Second)
+			seqCheckLink(l, c)
+		}(l)
 	}
-	et := time.Now()
-	fmt.Println("time taken for go Routine = ", et.Sub(st))
+	// et := time.Now()
+	// fmt.Println("time taken for go Routine = ", et.Sub(st))
 }
 
 //sequential
@@ -41,9 +44,9 @@ func seqCheckLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "down"
+		c <- link
 		return
 	}
 	fmt.Println(link, " is up")
-	c <- "up"
+	c <- link
 }
